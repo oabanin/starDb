a214796dd8cf3faf4efab5e627ae294ed329abb5
oabanin-HOME <juristaba@gmail.com>
2020-02-20 00:14:21 +0300

    componentDidCatch for single component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,17 +2,16 @@ import React from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
-import ItemList from '../item-list';
-import PersonDetails from '../person-details';
+
 import ErrorButton from '../error-button';
 import './app.css';
 import ErrorIndicator from '../error-indicator';
+import PeoplePage from '../people-page';
 
 export default class App extends React.Component {
 
   state = {
     showRandomPlanet: true,
-    selectedPerson: null,
     hasError: false 
   };
 
@@ -46,23 +45,14 @@ export default class App extends React.Component {
     return (
       <div>
         <Header />
-
         {planet}
-
         <button
           className="toggle-planet btn btn-warning btn-lg"
           onClick={this.toggleRandomPlanet}>
           Toggle Random Planet
         </button>
         <ErrorButton />
-        <div className="row mb2">
-          <div className="col-md-6">
-            <ItemList onItemSelected={this.onItemSelected} />
-          </div>
-          <div className="col-md-6">
-            <PersonDetails personId={this.state.selectedPerson} />
-          </div>
-        </div>
+        <PeoplePage/>
       </div>
     );
   };
