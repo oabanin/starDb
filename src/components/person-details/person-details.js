1a7284144f1391df6ba34fabd73273835a9220f3
oabanin-WORK <juristaba@gmail.com>
2020-02-19 16:46:34 +0400

    add lifecyclee methods componentDidUpdate and componentDidMount to PersonDetails

--- src/components/person-details/person-details.js
+++ src/components/person-details/person-details.js
@@ -3,39 +3,66 @@ import SwapiService from "../../services/swapi-service";
 import './person-details.css';
 
 export default class PersonDetails extends Component {
+
   swapiService = new SwapiService();
+
   state = {
     person: null
   }
 
-  updatePerson ()  {
-    const {personId} = this.props;
-    if(!personId) {
+  componentDidMount() {
+    this.updatePerson();
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    if(prevProps.personId !== this.props.personId){
+      this.updatePerson();
+    }
+    
+  }
+
+  updatePerson() {
+    const { personId } = this.props;
+    console.log(this.state);
+    if (!personId) {
       return
     }
+
+    
+    this.swapiService
+      .getPerson(personId)
+      .then((person) => {
+        this.setState({ person })
+      })
   }
 
   render() {
-    console.log(this.props);
+    if(!this.state.person){
+      return <span>Select person from a list</span>;
+    }
+
+    const { id, name, gender,
+                    birthYear, eyeColor } = this.state.person;
+
     return (
       <div className="person-details card">
         <img className="person-image"
-          src="https://starwars-visualguide.com/assets/img/characters/3.jpg" />
+          src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`} />
 
         <div className="card-body">
-          <h4>R2-D2</h4>
+          <h4>{name}</h4>
           <ul className="list-group list-group-flush">
             <li className="list-group-item">
               <span className="term">Gender</span>
-              <span>male</span>
+              <span>{gender}</span>
             </li>
             <li className="list-group-item">
               <span className="term">Birth Year</span>
-              <span>43</span>
+              <span>{birthYear}</span>
             </li>
             <li className="list-group-item">
               <span className="term">Eye Color</span>
-              <span>red</span>
+              <span>{eyeColor}</span>
             </li>
           </ul>
         </div>
