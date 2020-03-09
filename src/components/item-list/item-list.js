0eff94c3bb4c0a2fcf1707af9cefaca456896d49
oabanin-HOME <juristaba@gmail.com>
2020-03-10 00:11:22 +0300

    add prop-types react library for checking in components

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './item-list.css';
+import PropTypes from 'prop-types'
 
 // import { withData } from '../hoc-helpers';
 // const { getAllPeople } = new SwapiService();
@@ -28,6 +29,12 @@ const ItemList = (props) => {
 }
 ItemList.defaultProps = {
   onItemSelected: ()=>{}
+}
+
+ItemList.propTypes = {
+  onItemSelected: PropTypes.func, 
+  data: PropTypes.arrayOf(PropTypes.object).isRequired,
+  children: PropTypes.func.isRequired
 }
 
 export default ItemList;
--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -6,6 +6,7 @@ import SwapiService from '../../services/swapi-service';
 import Spinner from '../spinner';
 import ErrorIndicator from '../error-indicator';
 
+import PropTypes from 'prop-types';
 
 export default class RandomPlanet extends Component {
 
@@ -14,12 +15,7 @@ export default class RandomPlanet extends Component {
   }
 
   static propTypes = {
-    updateInterval: (props, propName, componentName) => {
-       const value = props[propName];
-       if(typeof value === 'number' && !isNaN(value)) return null;
-
-       return new TypeError(`${componentName}: ${propName} must be a number`)
-    }
+    updateInterval: PropTypes.number
   }
 
   swapiService = new SwapiService();
