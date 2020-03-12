5bc51050962f42ab3862d22e667fc850ed81db45
oabanin-HOME <juristaba@gmail.com>
2020-03-12 20:35:31 +0300

    add relative paths

--- src/components/pages/starship-page.js
+++ src/components/pages/starship-page.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { StarshipList } from '../sw-components';
+import { withRouter} from 'react-router-dom';
 
-
-const StarshipsPage = ()=> {
-        return (<StarshipList onItemSelected={()=>{}} />);
+const StarshipsPage = ({history})=> {
+        return (<StarshipList onItemSelected={(itemId)=>{
+                history.push(itemId);
+        }} />);
 }
 
-export default StarshipsPage;
+export default withRouter(StarshipsPage);  //working without withROuter
